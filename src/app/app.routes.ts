0429f3c41be5a10cdd4bdf8dc18460715cce2963
Auri <aurii.romero.sanchez@gmail.com>
2025-09-15 09:08:22 +0200

    Añadimos configuracion de rutas para el home, user, new user, and update user.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { UserDatailComponent } from './pages/user-datail/user-datail.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UptadeUserComponent } from './pages/uptade-user/uptade-user.component';
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'user/:id', component: UserDatailComponent},
+  { path: 'newuser', component: NewUserComponent},
+  { path: 'updateuser/:id', component: UptadeUserComponent},
+  { path: '**', redirectTo: '/home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
+
+
+
+
+
