63d2630dff32802da4d3fa15dbad333d5edbe591
Auri <aurii.romero.sanchez@gmail.com>
2025-09-17 09:19:53 +0200

    Añadido las funciones de borrar

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -15,6 +15,9 @@ import { Router } from '@angular/router';
 export class UsersComponent implements OnInit {
   users: User[] = [];
   loading = true;
+  showConfirmDeleteModal = false;
+  showSuccessModal = false;
+  userToDelete: User | null = null;
 
   constructor(private userService: UserService, private router: Router) {}
 
@@ -32,9 +35,32 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  onDeleteUser(id: number): void {
-    console.log('Delete user with id:', id);
-    // Aquí llamarías al servicio de usuario para borrar el usuario
+  promptDelete(id: number): void {
+    this.userToDelete = this.users.find(user => user.id === id) || null;
+    if (this.userToDelete) {
+      this.showConfirmDeleteModal = true;
+    }
+  }
+
+  cancelDelete(): void {
+    this.showConfirmDeleteModal = false;
+    this.userToDelete = null;
+  }
+
+  confirmDelete(): void {
+    if (this.userToDelete) {
+      // Here you would call the service to delete the user
+      // For now, just remove it from the local array
+      this.users = this.users.filter(user => user.id !== this.userToDelete!.id);
+      this.showConfirmDeleteModal = false;
+      this.showSuccessModal = true;
+      this.userToDelete = null;
+
+      // Hide success message after 2 seconds
+      setTimeout(() => {
+        this.showSuccessModal = false;
+      }, 2000);
+    }
   }
 
   onUpdateUser(id: number): void {
